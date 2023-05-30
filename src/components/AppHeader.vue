840eb25f154abe6cd160358af579ee82845f3deb
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-05-30 15:05:17 +0200

    inizio

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+    export default{
+        name: "AppHeader",
+
+        data (){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>ciao</div>
+</template>
+
+<style scoped>
+
+</style>
